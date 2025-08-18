930eb6a54b4a2d9fdc68ecc06d6cba5ef63358f0
Vanda Muradyan <vandamrdn@gmail.com>
2025-08-18 19:21:48 +0200

    added the full recipe-catalogue

--- lib/data/recipes.dart
+++ lib/data/recipes.dart
@@ -1,6 +1,96 @@
 // Jedes Rezept enthält die Gewürze mit ihren jeweiligen Mengen in Gramm
 
 const Map<String, Map<String, double>> recipes = {
+  "auberginen": {
+    "oregano": 0.0,
+    "paprika": 0.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 1.0,
+    "koriander": 0.0,
+    "chili": 1.0,
+  },
+
+  "baba_ganoush": {
+    "oregano": 0.0,
+    "paprika": 0.5,
+    "pfeffer": 0.5,
+    "kreuzkummel": 0.5,
+    "koriander": 0.0,
+    "chili": 0.0,
+  },
+
+  "barbacoa": {
+    "oregano": 1.0,
+    "paprika": 1.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 1.0,
+    "koriander": 1.0,
+    "chili": 0.0,
+  },
+
+  "cowboy_caviar": {
+    "oregano": 0.0,
+    "paprika": 0.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 0.5,
+    "koriander": 0.5,
+    "chili": 0.5,
+  },
+
+  "doener": {
+    "oregano": 1.0,
+    "paprika": 1.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 1.0,
+    "koriander": 0.0,
+    "chili": 0.0,
+  },
+
+  "empanadas": {
+    "oregano": 1.0,
+    "paprika": 0.0,
+    "pfeffer": 0.0,
+    "kreuzkummel": 1.0,
+    "koriander": 0.0,
+    "chili": 0.0,
+  },
+
+  "feta_aufstrich": {
+    "oregano": 1.0,
+    "pfeffer": 0.5,
+    "paprika": 0.0,
+    "kreuzkummel": 0.0,
+    "koriander": 0.0,
+    "chili": 0.0,
+  },
+
+  "feuertopf": {
+    "oregano": 1.0,
+    "paprika": 1.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 1.0,
+    "koriander": 0.0,
+    "chili": 0.5,
+  },
+
+  "ful_medames": {
+    "oregano": 0.0,
+    "paprika": 0.0,
+    "pfeffer": 0.0,
+    "kreuzkummel": 4.0,
+    "koriander": 0.0,
+    "chili": 2.0,
+  },
+
+  "koefte": {
+    "oregano": 0.0,
+    "paprika": 0.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 1.0,
+    "koriander": 1.0,
+    "chili": 1.0,
+  },
+
   "muhammara": {
     "paprika": 1.0,
     "kreuzkummel": 1.0,
@@ -19,13 +109,58 @@ const Map<String, Map<String, double>> recipes = {
     "chili": 0.0,
   },
 
-  "feta_aufstrich": {
+  "picadillo": {
     "oregano": 1.0,
+    "paprika": 1.0,
     "pfeffer": 0.5,
-    "paprika": 0.0,
-    "kreuzkummel": 0.0,
+    "kreuzkummel": 1.0,
     "koriander": 0.0,
     "chili": 0.0,
+  },
+
+  "puten_gyros": {
+    "oregano": 1.0,
+    "paprika": 1.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 1.0,
+    "koriander": 0.0,
+    "chili": 0.5,
+  },
+
+  "ropa_vieja": {
+    "oregano": 1.0,
+    "paprika": 1.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 1.0,
+    "koriander": 0.0,
+    "chili": 0.0,
+  },
+
+  "salsa_roja": {
+    "oregano": 0.0,
+    "paprika": 0.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 1.0,
+    "koriander": 0.0,
+    "chili": 1.0,
+  },
+
+  "tex_mex": {
+    "oregano": 0.0,
+    "paprika": 1.0,
+    "pfeffer": 0.0,
+    "kreuzkummel": 1.0,
+    "koriander": 0.0,
+    "chili": 1.0,
+  },
+
+  "tintenfisch": {
+    "oregano": 1.0,
+    "paprika": 0.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 0.0,
+    "koriander": 0.0,
+    "chili": 0.5,
   },
 
   "tomatensosse": {
@@ -35,6 +170,15 @@ const Map<String, Map<String, double>> recipes = {
     "kreuzkummel": 0.0,
     "koriander": 0.0,
     "chili": 0.0,
+  },
+
+  "zucchini_pfanne": {
+    "oregano": 0.0,
+    "paprika": 1.0,
+    "pfeffer": 0.5,
+    "kreuzkummel": 0.0,
+    "koriander": 0.0,
+    "chili": 0.5,
   },
 };
 
